7d523c1a3b76386f598c8cecd1b80b239c202979
MariamNsubuga <mariam.nakanyike@gmail.com>
2023-04-13 11:27:41 +0300

    UPDATE updating most files with content

--- afro_dj/afro_dtl/models.py
+++ afro_dj/afro_dtl/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# creating a custom user model
+class User_account(models.Model): 
+    username = models.CharField(max_length=22)
+    gender = models.CharField(max_length=6)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=22)
+    signup_time = models.DateTimeField(auto_now=True)
--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    pr_title = "Afro Django"  # variable to used in index file
+    username = 'Mariam'
+    gender = 'Female'
+    return render(request,
+                  'index.html',
+                  {'pr_title':pr_title, 'username':username, 'gender': gender})
+
+# register view
+def register(request):
+    return render(request, 'register.html')
+
+def registration(request):
+   username = request.POST['username']
+   password = request.POST['password']
+   email = request.POST['email']
+   gender = request.POST['gender']
+   user_details = [username , email, password, gender]
+   print (user_details)
+   return render(request, 'index.html',{'username':username})
+    
+  
